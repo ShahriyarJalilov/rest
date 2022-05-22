8e33d3fbc0017d2b7b2a4a811b0fa935d9f0dbed
Shahriyar <53611289+ShahriyarJalilov@users.noreply.github.com>
2022-05-22 07:59:00 -0700

    change

--- src/main/java/com/az/course/controller/AuthController.java
+++ src/main/java/com/az/course/controller/AuthController.java
@@ -25,7 +25,7 @@ public class AuthController {
     private final EmployeeService employeeService;
     private final JwtToken jwtToken;
     private final AuthenticationManager authenticationManager;
-    //v2
+
     @PostMapping("/token")
     public TokenResponse tokenResponse(@RequestBody TokenRequest request) {
         TokenResponse response = new TokenResponse(
@@ -52,7 +52,7 @@ public class AuthController {
 
     public  TokenResponse refreshToken(@RequestHeader ("Authorization") String token){
         TokenResponse response = new TokenResponse(
-                200,
+                0,
                 SUCCESS_MESSAGE.getMessage()
         );
         response.setToken(jwtToken.refreshToken(token));
